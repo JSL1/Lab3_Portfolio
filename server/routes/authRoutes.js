e073a690223cc279152b9729e36287e05c73b112
unknown <jeremy.st.pierre@gmail.com>
2026-08-09 02:11:13 -0400

    somet tests

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
-router.post("/login", authController.login);
-router.post("/signup", authController.signup);
+router.post("/login", (req, res, next) => {
+    console.log("LOGIN ROUTE HIT");
+    next();
+}, authController.login);
+
+router.post("/signup", (req, res, next) => {
+    console.log("SIGNUP ROUTE HIT");
+    next();
+}, authController.signup);
 
 module.exports = router;
 
