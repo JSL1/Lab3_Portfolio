71f007e58bd11e9299c6c15b16c5ee3fcac07bed
unknown <jeremy.st.pierre@gmail.com>
2026-08-08 21:07:11 -0400

    fixed more issues

--- client/src/Components/Users/Hello.js
+++ client/src/Components/Users/Hello.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../redux/authSlice";
+import { useSelector } from "react-redux";
 
 const Hello = () => {
 
     const dispatch = useDispatch();
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const user = useSelector(state => state.auth.user);
 
     const handleLogout = () => {
         dispatch(logout());
@@ -13,8 +16,14 @@ const Hello = () => {
 
     return(
         <div className="hello-user">
-            <div>Hello</div>
-            <div><Link to="./Login/">Log in</Link></div>
+            <div>Hello. {user?.firstname}</div>
+            <div>
+                {!isAuthenticated ? (
+                    <Link to="./Login/">Log In</Link>
+                ) : (
+                    <Link to="/" onClick={handleLogout}>Log Out</Link>
+                )}
+            </div>
         </div>
     );
 }
