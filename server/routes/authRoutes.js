03d8e7e7ac8aedd5aabc114ecccf426f8ae1c0fc
unknown <jeremy.st.pierre@gmail.com>
2026-08-08 22:17:55 -0400

    fixed some issues AGAIN

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
-router.post("login", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
 
